64b9b56735aabcb11e0acc082ae5835357b23965
NicoleG <guillen.nicole@pucp.edu.pe>
2022-04-12 15:15:50 -0500

    cambios realizados

---
+++ src/test/java/com/example/ejemplogit/EjemplogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemplogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplogitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
